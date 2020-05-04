e214bf34a05711b69d902261099624144a941de0
Ahmed Ibrahim <ahmedibrahim@Ahmeds-MacBook-Pro-2.local>
2020-05-04 22:11:28 +0200

    phase 2

---
+++ arraylist.h
@@ -0,0 +1,37 @@
+#ifndef ARRAYLIST_H
+#define ARRAYLIST_H
+
+/* GLOBALS */
+enum {  ALSUCCESS,
+        RMNODATA,
+        ALIDXOOR
+};
+
+typedef struct arrayStruct{
+    size_t data_size;   /* the size of the data being stored */
+    size_t max_length;  /* the current max length of the list */
+    size_t length;      /* the current length of the list */	
+	struct arrayStruct* prev;
+	struct arrayStruct* next[100];
+	int nextIndex;
+    void *array;
+    int (*add)();
+    int (*remove)();
+    void *(*get)();
+    int (*set)();
+    void (*free)();
+} ArrayList;
+
+ArrayList *newArrayList( size_t data_size );
+ArrayList *newArrayListSized( size_t data_size, size_t initial_length );
+int ArrayList__getoffset( ArrayList *list, int index );
+int ArrayList_add( ArrayList *list, void *data );
+int ArrayList_remove( ArrayList *list, int index );
+void ArrayList__realloc( ArrayList *list );
+void *ArrayList_get( ArrayList *list, int index );
+int ArrayList_set( ArrayList *list, int index, void *data );
+void ArrayList_free( ArrayList *list );
+char *ArrayList_toString( ArrayList *list );
+ArrayList* createNewScope(ArrayList * prevList, size_t data_size);
+    
+#endif
---
+++ structs.h
@@ -0,0 +1,37 @@
+#include <stdbool.h>
+
+typedef enum { Con, Id, Opr } nodeEnum;
+typedef enum { Int, Float, Char, Bool, String, ConstInt, ConstFloat, ConstChar, ConstBool, ConstString ,noType} typeEnum;
+
+/* constants */
+typedef struct {
+	typeEnum type;
+	char* value;
+} conNodeType;
+
+/* identifiers */
+typedef struct {
+    	typeEnum type;
+	int scope;
+	char* name;
+	bool used;
+	bool value;
+	bool declaration;
+} idNodeType;
+
+/* operators */
+typedef struct {
+    int oper;                   /* operator */
+    int nops;                   /* number of operands */	
+    struct nodeTypeTag *op[1];	/* operands, extended at runtime */
+} oprNodeType;
+
+typedef struct nodeTypeTag {
+    nodeEnum type;              /* type of node */
+
+    union {
+        conNodeType con;        /* constants */
+        idNodeType id;          /* identifiers */
+        oprNodeType opr;        /* operators */
+    };
+} nodeType;
